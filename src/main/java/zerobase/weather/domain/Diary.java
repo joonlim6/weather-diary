778a0da5edc00eca19ff648e9a67a95a2609d493
joonlim6 <limjoon629@gmail.com>
2023-08-29 22:57:39 +0900

    parse json response from openweathermap api + create diary object from parsed data

--- src/main/java/zerobase/weather/domain/Diary.java
+++ src/main/java/zerobase/weather/domain/Diary.java
@@ -8,13 +8,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDate;
 
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
-@Entity
 public class Diary {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    private String weather;
+    private String icon;
+    private double temperature;
+    private String text;
+    private LocalDate date;
 }
--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -1,28 +1,103 @@
 package zerobase.weather.service;
 
 import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import zerobase.weather.domain.Diary;
 import zerobase.weather.repository.DiaryRepository;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.time.LocalDate;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
 public class DiaryService {
     private final DiaryRepository diaryRepository;
 
-    @Value("{openweathermap.key}")
+    @Value("${openweathermap.key}")
     private String apiKey;
 
     @Transactional
     public void createDiary(LocalDate date, String text) {
+        String weatherData = getWeatherString();
+        System.out.println(weatherData);
+        Map<String, Object> parsedWeather = parseWeather(weatherData);
+
+        Diary nowDiary = new Diary();
+        nowDiary.setWeather(parsedWeather.get("main").toString());
+        nowDiary.setIcon(parsedWeather.get("icon").toString());
+        nowDiary.setTemperature((Double)parsedWeather.get("temp"));
+        nowDiary.setText(text);
+        nowDiary.setDate(date);
+
+        diaryRepository.save(nowDiary);
     }
 
     private String getWeatherString() {
         String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=" + apiKey;
-        System.out.println(apiUrl);
-        return "";
+
+        try {
+            BufferedReader br = getBufferedReader(apiUrl);
+
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+
+            return response.toString();
+        } catch (Exception e) {
+            return "failed to get response";
+        }
+    }
+
+    private static BufferedReader getBufferedReader(String apiUrl) throws IOException {
+        URL url = new URL(apiUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        BufferedReader br;
+
+        int responseCode = connection.getResponseCode();
+        if(responseCode == 200) {
+            br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        }   else {
+            br = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+        }
+        return br;
+    }
+
+    private Map<String, Object> parseWeather(String jsonString) {
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject;
+
+        try {
+            jsonObject = (JSONObject) jsonParser.parse(jsonString);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        Map<String, Object> resultMap = new HashMap<>();
+
+        JSONObject mainData = (JSONObject) jsonObject.get("main");
+        resultMap.put("temp", mainData.get("temp"));
+
+        JSONArray weatherArray = (JSONArray) jsonObject.get("weather");
+        JSONObject weatherData = (JSONObject) weatherArray.get(0);
+        resultMap.put("main", weatherData.get("main"));
+        resultMap.put("icon", weatherData.get("icon"));
+
+        return resultMap;
     }
 }
