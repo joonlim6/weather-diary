2f1045ab9dd1057286f859a44d3cd5723474873f
joonlim6 <limjoon629@gmail.com>
2023-08-29 17:21:23 +0900

    first commit

---
+++ src/main/java/zerobase/weather/repository/JdbcMemoRepository.java
@@ -0,0 +1,41 @@
+package zerobase.weather.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import zerobase.weather.domain.Memo;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class JdbcMemoRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public JdbcMemoRepository(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public Memo save(Memo memo) {
+        String insertSql = "insert into memo values(?, ?)";
+        jdbcTemplate.update(insertSql, memo.getId(), memo.getText());
+        return memo;
+    }
+
+    public List<Memo> findAll() {
+        String getAllSql = "select * from memo";
+        return jdbcTemplate.query(getAllSql, memoRowMapper());
+    }
+
+    public Optional<Memo> findById(int id) {
+        String getByIdSql = "select * from memo where id = ?";
+        return jdbcTemplate.query(getByIdSql, memoRowMapper(), id).stream().findFirst();
+    }
+
+    private RowMapper<Memo> memoRowMapper() {
+        return((rs, rowNum) -> new Memo(rs.getInt("id"), rs.getString("text")));
+    }
+}
