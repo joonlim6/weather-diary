0c6e109ffb85db33bac5cd441fad2f070d564d4c
joonlim6 <limjoon629@gmail.com>
2023-09-02 20:18:25 +0900

    Custom exception + global exception handler

--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -15,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import zerobase.weather.WeatherApplication;
 import zerobase.weather.domain.DateWeather;
 import zerobase.weather.domain.Diary;
+import zerobase.weather.exception.InvalidDateException;
 import zerobase.weather.repository.DateWeatherRepository;
 import zerobase.weather.repository.DiaryRepository;
 
@@ -81,7 +82,9 @@ public class DiaryService {
 
     @Transactional(readOnly = true)
     public List<Diary> readDiary(LocalDate date) {
-        logger.debug("read diary");
+        if (date.isAfter(LocalDate.ofYearDay(3050, 1))) {
+            throw new InvalidDateException();
+        }
         return diaryRepository.findAllByDate(date);
     }
 
